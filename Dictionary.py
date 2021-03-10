4d965c39ec9ddcb8b6e3f0b134e6cfef81520421
ihenriquez <ihenriquez86@gmail.com>
2021-03-10 15:58:39 -0300

    Dictionary from Neruda

---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+
+
+english_spanish = {"you":"tu", "can": "puedes", "cut": "cortar", "all": "todas", "the": "las", "flowers": "flores", "but": "pero", "cannot": "puedes", "keep": "mantener", "spring": "primavera", "from": "desde", "coming": "venir", "I": "yo", "grew": "crecio", "in" : "en", "this": "esta", "city": "ciudad", "my": "mi", "poetry": "poesía", "was": "fue", "born": "nacida", "between": "entre", "hill": "montaña", "and":"y", "river": "rio", "it": "eso", "took": "tomó", "its": "su", "voice": "voz", "rain": "lluvia", "like": "como", "timber": "madera", "stepped": "paró", "itself": "a si mismo", "in": "en", "forest": "bosque"}
+
+#  steeped itself in the forests.
---
+++ Neruda_Poem.py
@@ -0,0 +1,17 @@
+# This is a poem from Pablo Neruda
+
+# English version:
+# You can cut all the flowers but you cannot keep spring from coming.
+# I grew up in this town,
+# my poetry was born between the hill and the river,
+# it took its voice from the rain, and like the timber, it steeped itself in the forests.
+
+# Spanish version:
+# Podrán cortar todas las flores, pero no podrán detener la primavera.
+# Crecí en esta ciudad,
+# mi poesía nació entre la colina y el río,
+# tomó su voz de la lluvia, y como la madera, se empapó a sí misma en los bosques.
+
+# From the previous, I will take the english version and translate it to spanish without punctuation.
+
+
